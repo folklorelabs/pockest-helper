3dfa19d6091fd220afb9f38f707895b30f4e4fb8
Folklore Labs <git@folklorelabs.io>
2023-11-25 23:01:26 +0000

    cleaned up export sheet

--- scripts/parseMonsterData.js
+++ scripts/parseMonsterData.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const csvToJson = require('convert-csv-to-json');
 
-const MONSTER_CSV_PATH = './data/Pockest Helper - Monsters.csv';
+const MONSTER_CSV_PATH = './data/Pockest Helper - Export.csv';
 const DATA_DIR = './src/data';
 const MONSTER_FILE = 'monsters.json';
 
@@ -11,14 +11,8 @@ const MONSTER_FILE = 'monsters.json';
     .formatValueByType()
     .fieldDelimiter(',')
     .getJsonFromCsv(MONSTER_CSV_PATH);
-  const cleanMonsters = monsters.map((m) => ({
-    monster_id: m.monster_id,
-    name_en: m.name_en,
-    plan: m.plan,
-    matchFever: m.matchFever,
-  }));
   if (!await fs.existsSync(DATA_DIR)) {
     await fs.mkdirSync(DATA_DIR);
   }
-  await fs.writeFileSync(`${DATA_DIR}/${MONSTER_FILE}`, JSON.stringify(cleanMonsters, null, 2));
+  await fs.writeFileSync(`${DATA_DIR}/${MONSTER_FILE}`, JSON.stringify(monsters, null, 2));
 })();
