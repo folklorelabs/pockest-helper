ee699c0b45292183186d7e799ad34583f5c6b24a
Folklore Labs <git@folklorelabs.io>
2023-11-25 18:09:45 +0000

    add current fever target

--- src/components/MatchControls/index.jsx
+++ src/components/MatchControls/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {
   usePockestContext,
   pockestSettings,
+  getMonsterId,
 } from '../../contexts/PockestContext';
-import './index.css';
 import { parseDurationStr } from '../../utils/parseDuration';
 import useNow from '../../hooks/useNow';
 import MatchUpSelect from '../MatchUpSelect';
+import './index.css';
+import monsters from '../../data/monsters';
 
 function MatchControls() {
   const {
@@ -14,6 +16,16 @@ function MatchControls() {
     pockestDispatch,
   } = usePockestContext();
   const now = useNow();
+  const curMonsterId = React.useMemo(() => getMonsterId(pockestState), [pockestState]);
+  const matchTargets = React.useMemo(() => {
+    if (pockestState?.matchPreference) {
+      const targetMonster = monsters.find((m) => m.monster_id === pockestState?.matchPreference);
+      return targetMonster?.name_en;
+    }
+    const curMonster = monsters.find((m) => m.monster_id === curMonsterId);
+    const targets = curMonster?.matchFever?.map((id) => monsters.find((m) => m.monster_id === id));
+    return targets?.map((t) => t.name_en).join(', ');
+  }, [pockestState, curMonsterId]);
   const {
     data,
     autoMatch,
@@ -44,6 +56,14 @@ function MatchControls() {
         </span>
         <span className="PockestText PockestLine-value">
           {data?.monster?.exchange_time ? parseDurationStr(data.monster.exchange_time - now.getTime()) : '--'}
+        </span>
+      </div>
+      <div className="PockestLine">
+        <span className="PockestText">
+          Target
+        </span>
+        <span className="PockestText PockestLine-value">
+          {matchTargets ?? '--'}
         </span>
       </div>
     </div>
