65c499e28106e18cce21b01879fb74f298174a37
Folklore Labs <git@folklorelabs.io>
2023-12-05 05:12:10 +0000

    Fix issue with match discovery checker

--- src/utils/isMatchDiscovery.js
+++ src/utils/isMatchDiscovery.js
@@ -1,7 +1,7 @@
 export default function isMatchDiscovery(pockestState, result) {
-  const monsterId = (pockestState?.data?.monster?.hash?.split('-') || '')[0];
+  const monsterIdStr = (pockestState?.data?.monster?.hash?.split('-') || '')[0];
+  const monsterId = monsterIdStr ? parseInt(monsterIdStr, 10) : null;
   const monster = pockestState?.allMonsters.find((m) => m.monster_id === monsterId);
-  if (!result?.get_memento_point && !result?.get_egg_point) return false;
   const allMissing = [
     ...(monster?.matchSusFever || []),
     ...(monster?.matchUnknown || []),
