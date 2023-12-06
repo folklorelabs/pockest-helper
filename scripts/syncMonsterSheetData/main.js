5bb9d48ec82f3c09730cdf15b67c07199a1081f3
Folklore Labs <git@folklorelabs.io>
2023-12-06 03:56:05 +0000

    revert

--- scripts/syncMonsterSheetData/main.js
+++ scripts/syncMonsterSheetData/main.js
@@ -5,7 +5,7 @@ const uploadToCloud = require('./uploadToCloud');
 const MONSTER_FILE = 'monsters.json';
 const MONSTER_RANGE = 'Live!A:G';
 const HASHES_FILE = 'hashes.json';
-const HASHES_RANGE = 'LiveHashes!A:D';
+const HASHES_RANGE = 'LiveHashes!A:B';
 
 async function main() {
   const monsters = await fetchFromSheet(MONSTER_RANGE);
