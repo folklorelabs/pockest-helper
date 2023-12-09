93620cec722252767ac3d7d6dea1fad7fe7f16ce
folklorelabs <98740773+folklorelabs@users.noreply.github.com>
2023-12-09 11:23:20 -0800

    Account for edge cases with next poop timer (#58)

--- src/utils/getGarbageTimer.js
+++ src/utils/getGarbageTimer.js
@@ -3,17 +3,25 @@ import getStomachTimer from './getStomachTimer';
 export const GARBAGE_TIME = (2 * 60 * 60 * 1000);
 
 export default function getGarbageTimer(pockestState) {
+  if (typeof pockestState?.data?.monster?.stomach !== 'number') return null;
+  if (pockestState?.data?.monster?.stomach === 12) return null;
   const stomachTimer = getStomachTimer(pockestState);
   const nextSmall = pockestState?.data?.next_small_event_timer;
   if (stomachTimer && stomachTimer !== nextSmall) return nextSmall;
+  const now = (new Date()).getTime();
   const lastClean = pockestState?.log
-    .filter((entry) => entry.logType === 'cleaning')
     .reduce((recentCleanTs, entry) => {
+      if (entry.logType !== 'cleaning') return recentCleanTs;
       const newTs = entry?.timestamp;
-      const now = (new Date()).getTime();
-      if (now - newTs > GARBAGE_TIME) return recentCleanTs;
+      if (now - newTs > pockestState?.data?.monster?.liveTime) return recentCleanTs;
       return (newTs > recentCleanTs) ? newTs : recentCleanTs;
     }, null);
-  if (lastClean) return lastClean + GARBAGE_TIME;
+  let nextClean = lastClean + GARBAGE_TIME;
+  let nextStomach = pockestState.data.monster.stomach + 1;
+  while (nextStomach < 12 && nextStomach < now) {
+    nextClean += GARBAGE_TIME;
+    nextStomach += 1;
+  }
+  if (nextClean > now) return nextClean;
   return null;
 }
