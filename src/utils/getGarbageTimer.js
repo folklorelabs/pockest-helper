20ffcbf9fcdc39525b6016000861eb1de15a413b
Folklore Labs <git@folklorelabs.io>
2023-12-26 01:22:46 +0000

    Fix issue with missing next poop times

--- src/utils/getGarbageTimer.js
+++ src/utils/getGarbageTimer.js
@@ -1,6 +1,7 @@
 import getStomachTimer from './getStomachTimer';
 
 export const GARBAGE_TIME = (2 * 60 * 60 * 1000);
+export const GARBAGE_MAX = 12;
 
 export default function getGarbageTimer(pockestState) {
   if (typeof pockestState?.data?.monster?.stomach !== 'number') return null;
@@ -10,15 +11,20 @@ export default function getGarbageTimer(pockestState) {
   if (stomachTimer && stomachTimer !== nextSmall) return nextSmall;
   const now = (new Date()).getTime();
   const lastClean = pockestState?.log
-    .reduce((recentCleanTs, entry) => {
-      if (entry.logType !== 'cleaning') return recentCleanTs;
-      const newTs = entry?.timestamp;
-      if (now - newTs > pockestState?.data?.monster?.liveTime) return recentCleanTs;
-      return (newTs > recentCleanTs) ? newTs : recentCleanTs;
+    .reduce((latestTs, entry) => {
+      // filter out non-clean log entries
+      if (entry.logType !== 'cleaning') return latestTs;
+
+      // filter out old monster log entries
+      const potentialLatestTs = entry?.timestamp;
+      if (potentialLatestTs < pockestState?.data?.monster?.liveTime) return latestTs;
+
+      // return potentialLatestTs if later, otherwise keep old
+      return (potentialLatestTs > latestTs) ? potentialLatestTs : latestTs;
     }, null);
   let nextClean = lastClean + GARBAGE_TIME;
   let nextStomach = pockestState.data.monster.stomach + 1;
-  while (nextStomach < 12 && nextClean < now) {
+  while (nextStomach < GARBAGE_MAX && nextClean < now) {
     nextClean += GARBAGE_TIME;
     nextStomach += 1;
   }
