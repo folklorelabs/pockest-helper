614bc3c04406f58ca403279d466727c471453693
Folklore Labs <git@folklorelabs.io>
2024-01-03 08:12:31 +0000

    Use actual training time instead of crude estimate

--- src/utils/getMatchTimer.js
+++ src/utils/getMatchTimer.js
@@ -1,13 +1,16 @@
 import { MONSTER_LIFESPAN } from './getAgeTimer';
 
-// add 5 min for crude way to ensure we train first
-export const OPTIMAL_MATCH_TIME = MONSTER_LIFESPAN[3] + (5 * 60 * 1000);
+export const OPTIMAL_MATCH_TIME = MONSTER_LIFESPAN[3];
+export const TRAINING_THRESHOLD = 1000 * 60 * 60;
 
 export default function getMatchTimer(pockestState) {
   const monster = pockestState?.data?.monster;
   if (!monster) return null;
   const nextAvailableMatch = monster.exchange_time;
   if (!pockestState?.autoPlan) return nextAvailableMatch;
-  const optimalMatchTime = monster.live_time + OPTIMAL_MATCH_TIME;
-  return Math.max(optimalMatchTime, nextAvailableMatch);
+  return Math.max(
+    monster.live_time + OPTIMAL_MATCH_TIME,
+    monster.training_time,
+    nextAvailableMatch,
+  );
 }
