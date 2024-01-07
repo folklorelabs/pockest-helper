033870e09cc12ff405d1841ce31d1548f0f7ff01
Folklore Labs <git@folklorelabs.io>
2024-01-07 23:38:01 +0000

    Stun/death timer fix upon hatch

--- src/utils/getStomachEmptyTimer.js
+++ src/utils/getStomachEmptyTimer.js
@@ -4,6 +4,7 @@ export default function getStomachEmptyTimer(pockestState) {
   const stomachTimer = getStomachTimer(pockestState);
   if (!stomachTimer) return null;
   const stomach = pockestState?.data?.monster?.stomach;
+  if (stomach === 0) return stomachTimer - STOMACH_TIME;
   const stomachFullCount = Math.max(0, stomach - 1);
   const stomachFullTimerDiff = stomachFullCount * STOMACH_TIME;
   return stomachTimer + stomachFullTimerDiff;
