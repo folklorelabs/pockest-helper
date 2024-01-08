b221a0ee1da1c27fa563e9d85be61ce6bb1eed4d
Folklore Labs <git@folklorelabs.io>
2024-01-08 18:26:27 +0000

    Fix fallback next garbage timer logic

--- src/utils/getGarbageTimer.js
+++ src/utils/getGarbageTimer.js
@@ -5,12 +5,12 @@ export const GARBAGE_MAX = 12;
 
 export default function getGarbageTimer(pockestState) {
   if (typeof pockestState?.data?.monster?.stomach !== 'number') return null;
-  if (pockestState?.data?.monster?.stomach === 12) return null;
+  if (pockestState?.data?.monster?.stomach >= GARBAGE_MAX) return null;
   const stomachTimer = getStomachTimer(pockestState);
   const nextSmall = pockestState?.data?.next_small_event_timer;
   if (stomachTimer && stomachTimer !== nextSmall) return nextSmall;
   const now = (new Date()).getTime();
-  const lastClean = pockestState?.log
+  const lastClean = Math.max(pockestState?.data?.monster?.live_time, pockestState?.log
     .reduce((latestTs, entry) => {
       // filter out non-clean log entries
       if (entry.logType !== 'cleaning') return latestTs;
@@ -21,13 +21,8 @@ export default function getGarbageTimer(pockestState) {
 
       // return potentialLatestTs if later, otherwise keep old
       return (potentialLatestTs > latestTs) ? potentialLatestTs : latestTs;
-    }, null);
-  let nextClean = lastClean + GARBAGE_TIME;
-  let nextStomach = pockestState.data.monster.stomach + 1;
-  while (nextStomach < GARBAGE_MAX && nextClean < now) {
-    nextClean += GARBAGE_TIME;
-    nextStomach += 1;
-  }
-  if (nextClean > now) return nextClean;
-  return null;
+    }, null));
+  const garbageDiff = Math.ceil((now - lastClean) / GARBAGE_TIME);
+  console.log((new Date(lastClean)).toLocaleString(), garbageDiff);
+  return lastClean + (garbageDiff * GARBAGE_TIME);
 }
