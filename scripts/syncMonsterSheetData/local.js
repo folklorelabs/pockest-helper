faa6edbf7fc82183706de41fcf19e120c2844c09
Folklore Labs <git@folklorelabs.io>
2024-01-28 09:42:57 +0000

    remove unused scripts

--- scripts/syncMonsterSheetData/local.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const main = require('./main');
-
-(async () => {
-  const data = await main();
-  const output = Object.keys(data).map((k) => `${data[k]?.length} ${k}`);
-  console.log(`Successful sync! (${output.join(', ')})`);
-})();
