5dc45c74fee3a226c6f2a55418c08e2283fe50ea
Folklore Labs <git@folklorelabs.io>
2024-07-30 23:35:03 +0000

    Graceful error handling for all servicer worker calls

--- src/utils/fetchLatestRelease.js
+++ src/utils/fetchLatestRelease.js
@@ -1,7 +1,11 @@
 let cache;
-export default async function fetchAllHashes() {
+export default async function fetchLatestReleases() {
   if (cache) return cache;
-  const { release } = await chrome.runtime.sendMessage({ type: 'GET_LATEST_RELEASE' });
-  cache = release;
-  return release;
+  const data = await chrome.runtime.sendMessage({ type: 'GET_LATEST_RELEASE' });
+  if (data?.error) {
+    console.error(`${data.error}`);
+    return null;
+  }
+  cache = data?.release;
+  return data?.release;
 }
--- src/utils/postDiscord.js
+++ src/utils/postDiscord.js
@@ -1,5 +1,7 @@
 export default async function postDiscord(content) {
   const data = await chrome.runtime.sendMessage({ type: 'POST_DISCORD', content });
-  if (data?.message) console.error(`[postDiscord] ERROR: ${data.message}`);
+  if (data?.error) {
+    console.error(`${data.error}`);
+  }
   return data;
 }
