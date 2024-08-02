e455ce9289cbd10d9634b309becc6239e367fcee
Folklore Labs <git@folklorelabs.io>
2024-08-02 04:23:40 +0000

    simpler logic

--- scripts/zipExtension.js
+++ scripts/zipExtension.js
@@ -4,7 +4,7 @@ const { execSync } = require('node:child_process');
 const { version } = require('../package.json');
 
 const EXT_SRC = path.resolve(__dirname, '../dist');
-const EXT_FILE = 'PockestHelper_v[VERSION].zip';
+const EXT_FILE = `PockestHelper_v${version}.zip`;
 
 async function zip(srcDir, fileName) {
   const extensionZip = `${srcDir}/${fileName}`;
@@ -16,7 +16,6 @@ async function zip(srcDir, fileName) {
 }
 
 (async () => {
-  const fileName = EXT_FILE.replace('[VERSION]', version);
-  const fullFilePath = await zip(EXT_SRC, fileName);
+  const fullFilePath = await zip(EXT_SRC, EXT_FILE);
   console.log(fullFilePath);
 })();
