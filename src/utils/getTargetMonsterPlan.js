3c95ed7a650b31ac474c123d34c47ea9b290c26e
Folklore Labs <git@folklorelabs.io>
2024-08-03 11:56:11 +0000

    clean up

--- src/utils/getTargetMonsterPlan.js
+++ src/utils/getTargetMonsterPlan.js
@@ -1,6 +1,5 @@
 import { STAT_ABBR } from '../config/stats';
-import { LEGACY_EGG_IDS } from '../config/eggs';
-import ROUTES from '../config/routes';
+import { parsePlanId, LEGACY_PLAN_REGEX } from './parsePlanId';
 
 const PLAN_DEFAULTS = {
   cleanFrequency: null,
@@ -15,28 +14,6 @@ const PLAN_TIMES = [
   36 * 60 * 60 * 1000,
   168 * 60 * 60 * 1000,
 ];
-
-const LEGACY_PLAN_REGEX = /^([W|G|Y|B|R|L])([1-6])([A|B|C][L|R])([T|S|P])$/;
-const PLAN_REGEX = /^(\d*)([A|B|C][L|R])([T|S|P])([1-6])$/;
-
-export function parsePlanId(planId) {
-  const isLegacy = LEGACY_PLAN_REGEX.test(planId);
-  const planIdSplit = isLegacy ? LEGACY_PLAN_REGEX.exec(planId)
-    : PLAN_REGEX.exec(planId);
-  if (!planIdSplit) return null;
-  const planEggString = isLegacy ? `${LEGACY_EGG_IDS.indexOf(planIdSplit[1])}` : planIdSplit[1];
-  const primaryStatLetter = isLegacy ? planIdSplit[4] : planIdSplit[3];
-  const routeId = isLegacy ? planIdSplit[3] : planIdSplit[2];
-  const planAgeString = isLegacy ? planIdSplit[2] : planIdSplit[4];
-  return {
-    planId: `${planEggString}${routeId}${primaryStatLetter}${planAgeString}`,
-    planEgg: parseInt(planEggString, 10),
-    planRoute: ROUTES[routeId],
-    primaryStatLetter,
-    primaryStat: `${STAT_ABBR[primaryStatLetter]}`,
-    planAge: parseInt(planAgeString, 10),
-  };
-}
 
 export default function getTargetMonsterPlan(state) {
   const monster = state?.allMonsters?.find((m) => m.monster_id === state?.monsterId);
