d3e79ae44e77845b9c580baa13188b36e6864ec5
Folklore Labs <git@folklorelabs.io>
2024-08-05 17:40:38 +0000

    Fix for Chrome manifest and better output for release assets

--- scripts/xpiExtension.js
+++ scripts/xpiExtension.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 
 const { version } = require('../package.json');
 
-const SRC = path.resolve(__dirname, '../dist');
-const EXT_ASSET_NAME = `PockestHelper_v${version}.xpi`;
+const SRC = path.resolve(__dirname, '../artifacts');
+const EXT_ASSET_NAME = `PockestHelper${version.includes('rc') ? '_BETA' : ''}_v${version}.xpi`;
 
 (async () => {
   const fullFilePath = `${SRC}/${EXT_ASSET_NAME}`;
   const files = await fs.readdirSync(SRC);
-  const xpiFile = files.find((f) => /.*\.xpi$/.test(f));
-  await fs.renameSync(`${SRC}/${xpiFile}`, fullFilePath);
+  const xpiFiles = files.filter((f) => /.*\.xpi$/.test(f));
+  if (xpiFiles.length < 1) throw new Error('Unable to proceed. No .xpi file found.');
+  if (xpiFiles.length > 1) throw new Error('Unable to proceed. Multiple potential .xpi files found and unable to determine which to rename.');
+  await fs.renameSync(`${SRC}/${xpiFiles[0]}`, fullFilePath);
   console.log(fullFilePath);
 })();
--- scripts/zipExtension.js
+++ scripts/zipExtension.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const fs = require('fs');
 const { execSync } = require('node:child_process');
+const manifest = require('../dist/manifest.json');
 const { version } = require('../package.json');
 
 const EXT_SRC = path.resolve(__dirname, '../dist');
-const EXT_FILE = `PockestHelper_v${version}.zip`;
+const EXT_WD = path.resolve(__dirname, '../temp');
+const EXT_DEST = path.resolve(__dirname, '../artifacts');
+const EXT_FILE = `PockestHelper${version.includes('rc') ? '_BETA' : ''}_v${version}.zip`;
 
 async function zip(srcDir, fileName) {
   const extensionZip = `${srcDir}/${fileName}`;
@@ -15,7 +18,32 @@ async function zip(srcDir, fileName) {
   return extensionZip;
 }
 
+async function cleanUpManfiest(dir) {
+  const newManifest = JSON.parse(JSON.stringify(manifest));
+  delete newManifest.browser_specific_settings;
+  await fs.writeFileSync(`${dir}/manifest.json`, JSON.stringify(newManifest));
+}
+
 (async () => {
-  const fullFilePath = await zip(EXT_SRC, EXT_FILE);
-  console.log(fullFilePath);
+  // create dupe of src for work
+  await fs.cpSync(EXT_SRC, EXT_WD, { recursive: true });
+
+  // remove firefox specific manfiest settings
+  await cleanUpManfiest(EXT_WD);
+
+  // zip file
+  const fullFilePath = await zip(EXT_WD, EXT_FILE);
+
+  // move file to artifacts
+  const destFilePath = `${EXT_DEST}/${EXT_FILE}`;
+  if (!fs.existsSync(EXT_DEST)) {
+    await fs.mkdirSync(EXT_DEST);
+  }
+  await fs.renameSync(fullFilePath, destFilePath);
+
+  // clean up
+  await fs.rmSync(EXT_WD, { recursive: true, force: true });
+
+  // print abs file path
+  console.log(destFilePath);
 })();
