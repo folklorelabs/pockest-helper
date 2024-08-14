1cfd1a737a265520cb359bb5d85fbcb4472ea219
Folklore Labs <git@folklorelabs.io>
2024-08-14 03:08:04 +0000

    Rely on automated discord reporting and only enable it if there are fever matches. Also, added a button to manually submit a report in the event of a failed report

--- src/utils/postDiscord.js
+++ src/utils/postDiscord.js
@@ -1,7 +1,32 @@
 import logError from './logError';
 
+const DISCORD_ATTEMPT_TIMESTAMP_ID = 'PockestHelperTimeout-discordReportAttempt';
+const DISCORD_SUCCESS_TIMESTAMP_ID = 'PockestHelperTimeout-discordReportSuccess';
+const DISCORD_COOLDOWN = 1000 * 60;
+
+export function getDiscordCooldown() {
+  const lastDiscordAttemptStr = window.sessionStorage.getItem(DISCORD_ATTEMPT_TIMESTAMP_ID);
+  if (!lastDiscordAttemptStr) return null;
+  const lastDiscordAttempt = parseInt(lastDiscordAttemptStr, 10);
+  const now = Date.now();
+  const target = lastDiscordAttempt + DISCORD_COOLDOWN;
+  const diff = Math.max(0, Math.ceil((target - now) / 1000));
+  return diff;
+}
+
+export function getDiscordReportStatus() {
+  const lastDiscordAttemptStr = window.sessionStorage.getItem(DISCORD_ATTEMPT_TIMESTAMP_ID);
+  if (!lastDiscordAttemptStr) return true;
+  const lastDiscordSuccessStr = window.sessionStorage.getItem(DISCORD_SUCCESS_TIMESTAMP_ID);
+  if (!lastDiscordSuccessStr) return false;
+  const lastDiscordAttempt = parseInt(lastDiscordAttemptStr, 10);
+  const lastDiscordSuccess = parseInt(lastDiscordSuccessStr, 10);
+  return lastDiscordSuccess >= lastDiscordAttempt;
+}
+
 export async function postDiscord(content, url, id) {
   try {
+    window.sessionStorage.setItem(DISCORD_ATTEMPT_TIMESTAMP_ID, Date.now());
     if (!url) throw new Error(`Missing ${id} env var`);
     const response = await fetch(url, {
       body: JSON.stringify({ content }),
@@ -11,6 +36,7 @@ export async function postDiscord(content, url, id) {
       },
     });
     if (!response.ok) throw new Error(`API ${response.status} response (${id})`);
+    window.sessionStorage.setItem(DISCORD_SUCCESS_TIMESTAMP_ID, Date.now());
   } catch (err) {
     logError(`${err}`);
   }
