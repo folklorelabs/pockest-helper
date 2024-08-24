123455f0c61bc329c70553ae6982220efa3b6f2c
Folklore Labs <git@folklorelabs.io>
2024-08-24 00:53:49 +0000

    Remove target age 2-4 since they arent useful

--- src/components/TargetAgeSelect/index.jsx
+++ src/components/TargetAgeSelect/index.jsx
@@ -9,9 +9,9 @@ import {
 const AGE_INTERVAL = {
   6: '6 (Memento + Sticker)',
   5: '5 (Sticker)',
-  4: '4',
-  3: '3',
-  2: '2',
+  // 4: '4',
+  // 3: '3',
+  // 2: '2',
   1: '1 (Do nothing)',
 };
 
