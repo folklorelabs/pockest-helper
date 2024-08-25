176bfb32e1787f63e8c233f32a93e7934dced4fb
Folklore Labs <git@folklorelabs.io>
2024-08-25 17:59:40 +0000

    slightly better styling for error message

--- src/components/AppErrorAlert/index.jsx
+++ src/components/AppErrorAlert/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { usePockestContext } from '../../contexts/PockestContext';
 import './index.css';
+import { useAppContext } from '../../contexts/AppContext';
 
 function AppErrorAlert() {
   const {
     pockestState,
   } = usePockestContext();
+  const {
+    setShowLog,
+  } = useAppContext();
   if (!pockestState?.error) return '';
   return (
-    <div className="AppErrorAlert">
+    <button
+      className="AppErrorAlert"
+      type="button"
+      onClick={() => setShowLog(true)}
+    >
       <p className="AppErrorAlert-message">
         {pockestState?.error?.includes('403') && pockestState?.error?.includes('buckler') ? '403 Auth Error: Please ensure you are logged in' : pockestState?.error}
       </p>
-    </div>
+    </button>
   );
 }
 
