34f40bbf339b8f6fef48b7c409119afa5118e1f6
Folklore Labs <git@folklorelabs.io>
2024-08-29 09:18:33 +0000

    fixed potential issue with null start

--- src/contexts/PockestContext/getters.jsx
+++ src/contexts/PockestContext/getters.jsx
@@ -298,7 +298,7 @@ export function getCurrentPlanSchedule(state) {
 export function getMatchSchedule(state) {
   const birth = state?.data?.monster?.live_time;
   if (!birth) return [];
-  const targetDeath = MONSTER_AGE[Math.max(2, state?.planAge)];
+  const targetDeath = MONSTER_AGE[Math.max(2, state?.planAge || 6)];
   const firstMatchTime = state?.planAge < 4 ? birth : getFirstMatchTime(state);
   const numMatches = Math.max(0, Math.ceil((birth + targetDeath - firstMatchTime) / daysToMs(1)));
   const schedule = Array.from(new Array(numMatches)).map((v, i) => ({
