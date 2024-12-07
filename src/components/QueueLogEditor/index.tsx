0053e989e03e7423e4033f13cfa424c135ebf5d8
Folklore Labs <git@folklorelabs.io>
2024-12-07 07:50:05 +0000

    remove enabling of queue mode from log

--- src/components/QueueLogEditor/index.tsx
+++ src/components/QueueLogEditor/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   usePockestContext,
-  pockestActions,
+  // pockestActions,
 } from '../../contexts/PockestContext';
 import './index.css';
 import QueueList from '../QueueList';
@@ -9,7 +9,7 @@ import QueueList from '../QueueList';
 const QueueLogEditor: React.FC = () => {
   const {
     pockestState,
-    pockestDispatch,
+    // pockestDispatch,
   } = usePockestContext();
   return (
     <div className="QueueLogEditor">
@@ -21,7 +21,7 @@ const QueueLogEditor: React.FC = () => {
           {pockestState?.planQueue?.length || 0}
           )
         </p>
-        <label className="PockestCheck" htmlFor="PockestHelper_QueueLogEditorAutoQueue">
+        {/* <label className="PockestCheck" htmlFor="PockestHelper_QueueLogEditorAutoQueue">
           <input
             id="PockestHelper_QueueLogEditorAutoQueue"
             className="PockestCheck-input"
@@ -33,7 +33,7 @@ const QueueLogEditor: React.FC = () => {
             disabled={!pockestState.paused || (pockestState?.simpleMode && !!pockestState?.data?.monster?.live_time)}
           />
           <span className="PockestCheck-text">Queue</span>
-        </label>
+        </label> */}
       </header>
       <div className="QueueLogEditor-content">
         <QueueList />
