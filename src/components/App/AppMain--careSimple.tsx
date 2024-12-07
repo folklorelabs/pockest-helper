096b632cce3f48ef34d3ab361a18933832ef7f3f
Folklore Labs <git@folklorelabs.io>
2024-12-07 11:16:56 +0000

    simplify simple mode

--- src/components/App/AppMain--careSimple.tsx
+++ src/components/App/AppMain--careSimple.tsx
@@ -3,16 +3,22 @@ import AppMainTemplate from './AppMain';
 import SimplePlan from '../SimplePlan';
 import AutoPlanControlsSimple from '../AutoPlanControlsSimple';
 import QueueControls from '../QueueControls';
+import { usePockestContext } from '../../contexts/PockestContext';
 
 function AppMain() {
+  const {
+    pockestState,
+  } = usePockestContext();
   return (
     <AppMainTemplate
       className="AppMain--careSimple"
       content={(
         <>
-          <QueueControls />
-          <hr />
-          <AutoPlanControlsSimple />
+          {pockestState?.autoQueue ? (
+            <QueueControls />
+          ) : (
+            <AutoPlanControlsSimple />
+          )}
           <hr />
           <SimplePlan rows={45} />
         </>
