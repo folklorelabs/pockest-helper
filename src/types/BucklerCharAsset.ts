d9ee129170bdb54c2016997825dbfbda040da38a
Folklore Labs <git@folklorelabs.io>
2024-12-07 11:37:41 +0000

    fix sprites

--- src/types/BucklerCharAsset.ts
+++ src/types/BucklerCharAsset.ts
@@ -1,20 +1,12 @@
 import BucklerCharFrame from "./BucklerCharFrame";
 
 type BucklerCharAsset = {
-  frames: Record<string, BucklerCharFrame>;
-  animations: Record<string, string[]>;
-  meta: {
-    app: string;
-    version: string;
-    image: string;
-    format: string;
-    size: {
-      w: number;
-      h: number;
-    };
-    scale: string;
-    smartupdate: string;
-  };
+  image: string;
+  size: number;
+  attack: BucklerCharFrame[];
+  down: BucklerCharFrame[];
+  idle: BucklerCharFrame[];
+  win: BucklerCharFrame[];
 }
 
 export default BucklerCharAsset;
