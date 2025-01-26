cb988f652e67b30d6eab886c5a613f219d91c699
Folklore Labs <git@folklorelabs.io>
2025-01-26 08:13:11 +0000

    critical bugfix for training interval detection

--- src/contexts/PockestContext/getters/training.tsx
+++ src/contexts/PockestContext/getters/training.tsx
@@ -24,16 +24,17 @@ export function getTrainingIntervals(pockestState: PockestState): TrainingInterv
     trainingLogSchema,
     trainingSkipLogSchema,
   ])).safeParse(allTrainingLogs);
-  const plan = parsePlanId(pockestState.statPlanId);
+  const plan = parsePlanId(pockestState.planId);
   const trainingIntervals = Array.from(Array(numTrainings), (_, index) => {
     const start = monsterBirth + index * trainingWindow;
     const end = monsterBirth + (index + 1) * trainingWindow;
-    const statId = !pockestState?.autoPlan ? STAT_ID_ABBR[pockestState?.stat] : pockestState?.statPlanId.split('')[index] || plan?.primaryStat;
+    const statId = !pockestState?.autoPlan ? STAT_ID_ABBR[pockestState?.stat] : pockestState?.statPlanId.split('')[index] || plan?.primaryStatLetter;
     const stat = !statId ? undefined : parseInt(Object.keys(STAT_ID_ABBR)[Object.values(STAT_ID_ABBR).indexOf(statId)], 10);
+    const trainingLogs = parsedTrainingLogs.data?.filter((log) => log.timestamp >= start && log.timestamp < end) || [];
     return {
       start,
       end,
-      trainingLogs: parsedTrainingLogs.data?.filter((log) => start <= log.timestamp && end > log.timestamp) || [],
+      trainingLogs,
       stat,
       statId,
     };
