c6329e378ac106e336d7c6d330157f80c2bd266a
Folklore Labs <git@folklorelabs.io>
2025-06-21 18:56:53 -0700

    linting

--- src/components/QueueList/SortableQueueList.tsx
+++ src/components/QueueList/SortableQueueList.tsx
@@ -17,22 +17,22 @@ function SortableQueueList() {
         items={pockestState.presetQueue}
         ItemComponent={QueueItem}
         onDragEnd={(event) => {
-        const {active, over} = event;
-        if (!over) return;
-        if (active.id === over.id) return;
-        if (over.disabled) return;
-        const curIndex = active?.data?.current?.sortable.index;
-        const newIndex = over?.data?.current?.sortable.index;
-        if (typeof newIndex !== 'number' || typeof curIndex !== 'number') return;
-        const presetQueue: PresetQueueItem[] = [
-            ...pockestState.presetQueue.slice(0, curIndex),
-            ...pockestState.presetQueue.slice(curIndex + 1),
-        ];
-        const itemToMove = pockestState.presetQueue[curIndex];
-        presetQueue.splice(newIndex, 0, itemToMove);
-        pockestDispatch?.(pockestActions.pockestPlanSettings({
-            presetQueue,
-        }));
+          const {active, over} = event;
+          if (!over) return;
+          if (active.id === over.id) return;
+          if (over.disabled) return;
+          const curIndex = active?.data?.current?.sortable.index;
+          const newIndex = over?.data?.current?.sortable.index;
+          if (typeof newIndex !== 'number' || typeof curIndex !== 'number') return;
+          const presetQueue: PresetQueueItem[] = [
+              ...pockestState.presetQueue.slice(0, curIndex),
+              ...pockestState.presetQueue.slice(curIndex + 1),
+          ];
+          const itemToMove = pockestState.presetQueue[curIndex];
+          presetQueue.splice(newIndex, 0, itemToMove);
+          pockestDispatch?.(pockestActions.pockestPlanSettings({
+              presetQueue,
+          }));
         }}
     />
   );
