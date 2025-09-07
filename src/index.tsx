af7ca9d34d34785f99f51c6ff55a9935adfdff14
Folklore Labs <git@folklorelabs.io>
2025-09-07 11:04:20 -0700

    add hook for easier discord testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import { AppProvider } from './contexts/AppProvider';
-import { PockestProvider } from './contexts/PockestContext';
-import log from './utils/log';
-import './reset.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { AppProvider } from "./contexts/AppProvider";
+import { PockestProvider } from "./contexts/PockestContext";
+import log from "./utils/log";
+import testDiscord from "./utils/testDiscord";
+import "./reset.css";
+import "./index.css";
 
-const APP_ID = 'PockestHelperExtension';
+const APP_ID = "PockestHelperExtension";
 
-const appContainer = document.querySelector('body');
+const appContainer = document.querySelector("body");
 let mainEl = document.getElementById(APP_ID);
 if (mainEl && appContainer) {
-  log('Existing Pockest Helper instance found. Removing the element.');
-  appContainer.removeChild(mainEl);
+	log("Existing Pockest Helper instance found. Removing the element.");
+	appContainer.removeChild(mainEl);
 }
-mainEl = document.createElement('div');
+mainEl = document.createElement("div");
 mainEl.id = APP_ID;
 if (appContainer) {
-  appContainer.appendChild(mainEl);
+	appContainer.appendChild(mainEl);
 }
 ReactDOM.createRoot(mainEl).render(
-  <React.StrictMode>
-    <PockestProvider>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </PockestProvider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<PockestProvider>
+			<AppProvider>
+				<App />
+			</AppProvider>
+		</PockestProvider>
+	</React.StrictMode>,
 );
+
+if (import.meta.env.DEV) testDiscord();
